135a13038f2d8baded89e62c624b32e253be5625
arttu <ttu91@hotmail.com>
2025-06-20 21:27:28 +0300

    add db

---
+++ backend/src/database.ts
@@ -0,0 +1,16 @@
+import { Kysely, PostgresDialect } from 'kysely';
+import { Pool } from 'pg';
+
+export interface Database {
+  // Add your table interfaces here
+}
+
+const dialect = new PostgresDialect({
+  pool: new Pool({
+    connectionString: process.env.DATABASE_URL,
+  }),
+});
+
+export const db = new Kysely<Database>({
+  dialect,
+});
