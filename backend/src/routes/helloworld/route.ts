6dbb78a383e3da57831602b9cc8879c83ff82f68
arttu <ttu91@hotmail.com>
2025-06-22 16:49:47 +0300

    delete helloworld

--- backend/src/routes/helloworld/route.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyInstance } from 'fastify';
-import { getHello } from './service';
-
-export async function helloworldRoute(fastify: FastifyInstance) {
-  fastify.get('/', async (_request, _reply) => {
-    return getHello();
-  });
-}
--- backend/src/routes/helloworld/service.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from "vitest";
-import { getHello } from "./service";
-
-describe("getHello", () => {
-  it('should return "Hello world"', () => {
-    const result = getHello();
-    expect(result).toBe("Hello world");
-  });
-});
